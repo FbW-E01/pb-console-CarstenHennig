871ef34b8b5a26ec590f11a3686661d05868a35d
CarstenHennig <carsten.hennig@gmail.com>
2021-03-29 13:54:24 +0200

    Some variables

--- start.js
+++ start.js
@@ -7,10 +7,16 @@ let firstName = "Carsten";
 let lastName ="Hennig";
 console.log(firstName, lastName);
 
-const fullName = "Carsten Hennig";
-const city = "Hamburg";
-const country = "Germany";
-const job = "Director of Communications";
-const status = "online";
-console.log(fullName, " is from ", city, country, " and is working as a ", job, " and now ", status, ".");
+let fullName = "Carsten Hennig";
+let city = "Hamburg";
+let country = "Germany";
+let job = "Director of Communications";
+let status = "online";
+console.log(fullName, " is from ", city, "/", country, " and is working as a ", job, " and now ", status, ".");
+
+city = "Berlin";
+status = "offline";
+console.log(fullName, " is from ", city, "/", country, " and is working as a ", job, " and now ", status, ".");
+console.log("Test");
+
 
