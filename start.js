873d42bc0a02d68f56667bc16f1ba7df85e7c5bb
CarstenHennig <carsten.hennig@gmail.com>
2021-03-29 14:01:04 +0200

    Some more variables

--- start.js
+++ start.js
@@ -12,11 +12,17 @@ let city = "Hamburg";
 let country = "Germany";
 let job = "Director of Communications";
 let status = "online";
-console.log(fullName, " is from ", city, "/", country, " and is working as a ", job, " and now ", status, ".");
+console.log(fullName, "is from", city, "/", country, "and is working as a", job, "and now", status, ".");
 
 city = "Berlin";
 status = "offline";
-console.log(fullName, " is from ", city, "/", country, " and is working as a ", job, " and now ", status, ".");
-console.log("Test");
+console.log(fullName, "is from", city, "/", country, "and is working as a", job, "and now", status, ".");
+
+fullName = "Abraxas St. Germain";
+job = "conductor";
+city = "Munich";
+status = "lost in space";
+console.log(fullName, "is from ", city, "/", country, "and is working as a", job, "and now", status, ".");
+
 
 
