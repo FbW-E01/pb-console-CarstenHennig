d1ee08eda5d24f9f573f36d64be0a31def713422
CarstenHennig <carsten.hennig@gmail.com>
2021-03-29 13:42:59 +0200

    Some JS variables

---
+++ start.js
@@ -0,0 +1,16 @@
+console.log("This is an exercise in console logging");
+
+let value = "This is a value.";
+console.log(value);
+
+let firstName = "Carsten";
+let lastName ="Hennig";
+console.log(firstName, lastName);
+
+const fullName = "Carsten Hennig";
+const city = "Hamburg";
+const country = "Germany";
+const job = "Director of Communications";
+const status = "online";
+console.log(fullName, " is from ", city, country, " and is working as a ", job, " and now ", status, ".");
+
